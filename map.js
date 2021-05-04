d6520f62407183b247225cb4d865816d33a431b3
salitosmbogz <salitosmbogz@gmail.com>
2021-05-04 16:27:12 +0300

    Added Recursive case that leverages Higher Order Functions

--- map.js
+++ map.js
@@ -1,21 +1,39 @@
 // you are not allowed to use a for loop
 
 // code
-const map = (callback) => (arr) => {
+const map = (callback) =>(start_position)=> (arr) => {
   /* exit case */
   // check if we reached the end of the array.
   // if you have reached it, return.
   /* recursive case */
   // here, we know we are not at the end.
   // we are going to call map with the next value.
+
+  /*recursive case: 
+  through each iteration, the index of element we modify and the array object are going to change,
+  but the callback function will remain the same*/
+  if(arr.length-start_position > 0){
+    arr[start_position]=callback(arr[start_position]);
+
+    return add1ToEach(++start_position)(arr);
+
+  }
+
+  //exit case
+  else return arr;
+
 };
+
 
 const add1ToEach = map((item) => item + 1);
 
+const add1ToEachFromZero = add1ToEach(0)
+
+const actual = add1ToEachFromZero([1, 2, 3]);
 // tests
-const actual = add1ToEach([1, 2, 3]);
 const expected = [2, 3, 4];
 console.log(`expected: ${expected}\nactual: ${actual} \n`);
+
 
 /* iterative version */
 // const result = [];
